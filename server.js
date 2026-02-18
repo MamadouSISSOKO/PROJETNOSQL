d6be566da8e45a416f8001a8a271b68b0e8e47c5
MamadouSISSOKO <sissokomohamed1999@gmail.com>
2026-02-18 10:45:42 +0100

    schema

--- server.js
+++ server.js
@@ -1,31 +1,12 @@
-require('dotenv').config({ debug: process.env.DEBUG });
+require("dotenv").config();
 
-const http = require('http');
-
-const app = require('./app');
-
-
-
-app.use(function(req, res, next) {
-
-    res.header("Access-Control-Allow-Origin", "*");
-
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-
-    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-
-    next();
-
-});
+const http = require("http");
+const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
-
-    console.log('Server up and running on port ' + PORT);
-
+  console.log(`🚀 Server running on port ${PORT}`);
 });
--- swagger.js
+++ swagger.js
@@ -13,8 +13,41 @@ const options = {
         url: "https://projetnosql-q73t.onrender.com",
       },
     ],
+    // AJOUT DES COMPOSANTS (SCHÉMAS)
+    components: {
+      schemas: {
+        User: {
+          type: "object",
+          required: ["username", "password"], // Champs obligatoires
+          properties: {
+            id: {
+              type: "string",
+              description: "L'identifiant unique généré par MongoDB",
+            },
+            username: {
+              type: "string",
+              description: "Le nom de l'utilisateur",
+            },
+            password: {
+              type: "string",
+              description: "Le mot de passe (haché)",
+            },
+            role: {
+              type: "string",
+              enum: ["admin", "user"],
+              default: "user",
+            }
+          },
+          example: {
+            username: "RobbieLens",
+            password: "monPasswordSecurise123",
+            role: "user"
+          }
+        }
+      }
+    }
   },
-  apis: ["./routes/*.js"], // adapte si besoin
+  apis: ["./Routes/*.js"], // Vérifie bien que c'est "Routes" avec une majuscule !
 };
 
 module.exports = swaggerJsdoc(options);
